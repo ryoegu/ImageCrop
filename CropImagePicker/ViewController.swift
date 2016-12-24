d8d9986544ac08dbcedb1e9230fc70775268daee
Ryo Eguchi <ryo@ryos-macbook-pro.local>
2016-12-24 13:50:24 +0900

    Initial Commit

---
+++ CropImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CropImagePicker
+//
+//  Created by Ryo Eguchi on 2016/12/24.
+//  Copyright © 2016年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
